c726c2887b5977725105590f986a50447ddb1cc6
Martin Ben <mebenav88@gmail.com>
2015-11-08 13:53:03 -0500

    add board show action controller and show view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   before_action :all_boards, only: [:index, :create]
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
   respond_to :html, :js
   # def index
   #   @boards = Board.all
@@ -16,12 +17,19 @@ class BoardsController < ApplicationController
     @board = Board.create(board_params)
   end
 
+  def show
+  end
+
   private
     def all_boards
       @boards = Board.all
     end
-    
+
     def board_params
       params.require(:board).permit(:title)
+    end
+
+    def find_board
+      @board = Board.find(params[:id])
     end
 end
