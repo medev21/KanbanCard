d274b10ab50766dcd44ad53c848c0975bbc191af
Martin Ben <mebenav88@gmail.com>
2015-11-08 00:33:05 -0500

    add new create action on board controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,17 @@
 class BoardsController < ApplicationController
   def index
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:title)
+    end
 end
