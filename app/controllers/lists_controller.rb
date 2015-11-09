6cd67a27de026a8e1d6551e13a5c487108a4f10e
Martin Ben <mebenav88@gmail.com>
2015-11-09 14:42:56 -0500

    add list create controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,16 @@
 class ListsController < ApplicationController
+  before_action :find_board
+
+  def create
+    @list = @board.lists.create(list_params)
+  end
+
+  private
+    def find_board
+      @board = Board.find(params[:board_id])
+    end
+
+    def list_params
+      params.require(:list).permit(:listTitle)
+    end
 end
