3dac5ce410a624e4124f17dd6efa1ee1b7af93e6
Martin Ben <mebenav88@gmail.com>
2016-03-02 22:32:57 -0500

    add model relationship

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ActiveRecord::Base
+  belongs_to :board
+  belongs_to :user
+
+  belongs_to :list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301010519) do
+ActiveRecord::Schema.define(version: 20160301012843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160301010519) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "board_id"
   end
 
   create_table "lists", force: :cascade do |t|
