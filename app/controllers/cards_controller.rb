cbac6381c2b09eb740010d4adb33dbaf5b3777ba
Martin Ben <mebenav88@gmail.com>
2016-03-02 22:49:17 -0500

    add create update destroy actions to Card controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,37 @@
 class CardsController < ApplicationController
+
+  def create
+    @card = @board.cards.create(card_params)
+    @card.user_id = current_user.id
+    @card.save
+
+    redirect_to @board
+  end
+
+  def update
+    @card.update_attributes(card_params)
+    respond_with_bip(@card)
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to @board
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:title)
+    end
+
+    def find_board
+      @board = Board.find(params[:board_id])
+    end
+
+    def find_list
+      @list = List.find(params[:list_id])
+    end
+
+    def find_card
+      @card = Card.find(params[:id])
+    end
 end
